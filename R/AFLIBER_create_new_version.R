f6a4353c019c0dccd319c90ffa3d5d7e9fc57fab
Ignacio <ignacio.ramosgutierrez@uam.es>
2025-03-09 14:36:36 +0100

    compilation process... References

--- R/AFLIBER_create_new_version.R
+++ R/AFLIBER_create_new_version.R
@@ -2,7 +2,7 @@ library(here)
 library(tidyverse)
 
 
-
+source(here::here("R/AFLIBER_functions.R"))
 
 #1.  Species List re-generation
 
@@ -12,18 +12,22 @@ AFLIBER_specieslist_old <- readr::read_csv("inst/AFLIBER/raw/AFLIBER_Species_lis
 
 powonames <- rWCVPdata::wcvp_names
 
+
 AFLIBER_specieslist_new <- AFLIBER_specieslist_old  |> 
   # bind_rows(AFLIBER_newtaxa)
   # removing deprecated names
-  mutate(GBIF_id = sapply(AFLIBER_specieslist_new$Scientific_Name, get_gbif_id))
-
+  mutate(GBIF_id = sapply(Scientific_Name, get_gbif_id))
+  
 AFLIBER_specieslist_new <- AFLIBER_specieslist_new |> 
-  mutate(POW_Name = sapply(AFLIBER_specieslist_new$Taxon, get_POWO_name, powonames = powonames))
+  mutate(POW_Name = sapply(Taxon, get_POWO_name, powonames = powonames))
+
+readr::write_csv(AFLIBER_specieslist_new, "AFLIBER_v2.0.0/AFLIBER_Species_list.csv")
 
 
 
 #2. Distribution dataset compilation
 
+# Load old dataset
 AFLIBER_distributions_old <- readr::read_csv("inst/AFLIBER/raw/AFLIBER_Distributions.csv", locale = locale(encoding = "Latin1")) |> 
   separate_longer_delim(References, delim = "_") |> 
   rename(UTM10x10 = UTM.cell) |> 
@@ -34,8 +38,9 @@ AFLIBER_distributions_old <- readr::read_csv("inst/AFLIBER/raw/AFLIBER_Distribut
          References)
 
 
+ # Merge new datasets
 citkeys <- list.dirs("inst", full.names = F, recursive = F) 
-citkeys <- citkeys[!citkeys %in% c("AFLIBER", "ERRORS")]
+citkeys <- citkeys[!citkeys %in% c("AFLIBER", "ERRORS", "COMPILATION")]
 
 AFLIBER_novelties <- data.frame(Taxon      = NULL,
                                 UTM1x1     = NULL,
@@ -49,15 +54,48 @@ for(citkey in citkeys){
 
   if(citkey == "Mateo2024"){
     newdata <- newdata |>
-      mutate(References = "Mateo24")
+      mutate(References = "Mateo, G. (2024). Personal Database.")
   }
   AFLIBER_novelties <- AFLIBER_novelties |> 
     bind_rows(newdata)
 }
 
-AFLIBER_novelties [is.na(AFLIBER_novelties$References),]
+
+
+ # Amend Reference errors
+source("inst/COMPILATION/Compilation_functions.R")
 refs <- AFLIBER_novelties |> 
-  distinct(References, branch)
+  distinct(References)
+
+ref_dictionary <- readxl::read_excel(here::here("inst/COMPILATION/reference_correction_subtitutions.xlsx"))|> 
+  mutate(ref_grep_pattern =gsub(" ", " ", ref_grep_pattern),
+         substitution =gsub(" ", " ", substitution))
+
+refs <- refs |> 
+  mutate(Reference_ok =NA) |> 
+  mutate(References =gsub(" ", " ", References),
+         Reference_ok =gsub(" ", " ", Reference_ok))
 
 
+for(i in 1:nrow(ref_dictionary)){
+  
+  refs <- substitute_refs(data = refs, 
+                          dictionary = ref_dictionary[i,], 
+                          col.patt ="ref_grep_pattern",
+                          col.sub  ="substitution",
+                          exact.col ="exact"
+  )
+}
+refs <- refs |> 
+  mutate(Reference_ok = ifelse(is.na(Reference_ok), References, Reference_ok)) |> 
+  distinct()
+
+ AFLIBER_novelties |> 
+  left_join(refs) |> 
+  select(-References) |> 
+  rename(References = Reference_ok) |> 
+  select(Taxon,
+         UTM1x1,
+         UTM10x10,
+         References)
 
